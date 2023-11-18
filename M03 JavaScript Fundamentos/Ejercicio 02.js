4dd9ae0db1008cd793868eb72ff9d1a60e0748ac
David <davidalejandroalvarez2000@gmail.com>
2023-11-18 01:46:42 -0300

    M03 y M04 terminados

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,46 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   if (typeof x !== 'number' || typeof y !== 'number') {
+      return "Ambos parametros deben ser numeros";
+   }
+   return (x + y);
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   if (typeof x !== 'number' || typeof y !== 'number') {
+      return "Ambos parametros deben ser numeros";
+   }
+   return (x - y);
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   if (typeof x !== 'number' || typeof y !== 'number') {
+      return "Ambos parametros deben ser numeros";
+   }
+   return (x / y);
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   if (typeof x !== 'number' || typeof y !== 'number') {
+      return "Ambos parametros deben ser numeros";
+   }
+   return (x * y);
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   if (typeof x !== 'number' || typeof y !== 'number') {
+      return "Ambos parametros deben ser numeros";
+   }
+   return (x % y);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
